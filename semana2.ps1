b8262ef1f126982fececf5db61f84568eb2deb0f
tobias <tobiasmesa03@gmail.com>
2022-05-23 14:30:10 -0300

    asa

---
+++ semana2.ps1
@@ -0,0 +1,13 @@
+﻿$vcenter = "192.168.12.60"
+$user = "tmesa"
+$password = "Wetcom01"
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+
+Connect-VIServer -Server $vcenter -User $user -Password $password
+
+$todayDate = Get-Date
+$snapshots = Get-VM | Get-Snapshot| Where {$_.Created -lt (Get-Date).AddDays(-7)}
+
+$snapshots | Select-Object VM, created 
