2d3a45c682ccafcfa83c64fea1d6891eaac99c95
Tobias Mesa <83445093+tobiasmesa@users.noreply.github.com>
2022-06-02 09:53:36 -0300

    Update semana4.ps1
    Mejorando el output

--- semana4.ps1
+++ semana4.ps1
@@ -3,7 +3,7 @@ $data = @()
 foreach ($job in $jobs) 
 {
   $lastResult = $job.GetLastResult()
-  $data += $job | select Name, @{n='Ultimo status';e={$lastResult}}
+  $data += $job | select @{n='Name'; e={$job.Name}}, @{n='Ultimo status';e={$lastResult}}
 }
 
 $data | export-csv C:\data.csv 
