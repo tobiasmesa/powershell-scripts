62f0e7d584e649b33d68a4b1adedc94682805dc3
tobias <tobiasmesa03@gmail.com>
2022-06-03 10:06:37 -0300

    Adding sem2

---
+++ semana2.ps1
@@ -0,0 +1,7 @@
+﻿Connect-VIServer -Server $vcenter -User $user -Password $password
+
+
+$snapshots = Get-VM | Get-Snapshot| Where {$_.Created -lt (Get-Date).AddDays(-7)} | Select-Object VM, created | Export-Csv -Path C:\miData.csv
+
+$snapshots | Select-Object VM, created 
+Import-Csv -Path C:\miData.csv
