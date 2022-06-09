339dfccf18803672ea41e3cdb052551b60cebbc9
Tobias Mesa <83445093+tobiasmesa@users.noreply.github.com>
2022-06-09 10:43:40 -0300

    Update semana5.ps1

--- semana5.ps1
+++ semana5.ps1
@@ -2,12 +2,15 @@ $user="tmesa"
 $pass="Wetcom01"
 $vcenter="192.168.12.60"
 Connect -VIServer -Server$vcenter -User$user -Password$pass
-                              -InvalidCertificateAction Ignore -Confirm:$false Set-PowerCLIConfiguration
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP$false -Confirm:$false
+
 $VMs=Get-VM
 $DateToCompare=(Get-Date).AddDays(-365)
+
 $restorePoints=$VMs|ForEach-Object{
     Get-VBRRestorePoint -Name$_.Name|
          Where-Object{$_.CreationTime -gt$DateToCompare}
 }|Select-Object VMName,Creation Time
+
 $restorePoints|Group-Object VMName Select-Object Name,Count|Export-Csv.\output.csv'-NoType
